df9aea57d771318e3151edf3ceacb2b40ede8322
YoriDigitalent <yori.digitalent@gmail.com>
2020-10-18 22:39:41 +0700

    using utils wrapper

--- menu-service/handler/menu.go
+++ menu-service/handler/menu.go
@@ -1,21 +1,13 @@
 package handler
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
+
+	"github.com/YoriDigitalent/digitalentMicroservice/utils"
 )
 
 // AddMenu handle add menu
 func AddMenu(w http.ResponseWriter, r *http.Request) {
-	response, err := json.Marshal(map[string]interface{}{
-		"success": true,
-	})
 
-	if err != nil {
-		fmt.Printf("Failed generate response %s", err)
-		return
-	}
-
-	w.Write(response)
+	utils.WrapAPISuccess(w, r, "success", 200)
 }
