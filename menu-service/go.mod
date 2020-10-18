1b56552be16257f674e8e4583dd8a0a355f95916
YoriDigitalent <yori.digitalent@gmail.com>
2020-10-18 22:58:52 +0700

    init config

--- menu-service/go.mod
+++ menu-service/go.mod
@@ -2,4 +2,8 @@ module github.com/YoriDigitalent/digitalentMicroservice/menu-service
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/YoriDigitalent/digitalentMicroservice/utils v0.0.0-20201018153239-8a6da5dcbe83
+	github.com/gorilla/mux v1.8.0
+	github.com/spf13/viper v1.7.1
+)
--- menu-service/main.go
+++ menu-service/main.go
@@ -5,15 +5,43 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/YoriDigitalent/digitalentMicroservice/menu-service/config"
 	"github.com/YoriDigitalent/digitalentMicroservice/menu-service/handler"
 	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
 )
 
 func main() {
+
+	cfg, err := getConfig()
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
 	router := mux.NewRouter()
 
 	router.Handle("/add-product", http.HandlerFunc(handler.AddMenu))
 
-	fmt.Println("Server listen on :8000")
-	log.Panic(http.ListenAndServe(":8000", router))
+	fmt.Printf("Server listen on :%s", cfg.Port)
+	log.Panic(http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), router))
+}
+
+func getConfig() (config.Config, error) {
+
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+	viper.SetConfigName("config.yml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return config.Config{}, err
+	}
+
+	var cfg config.Config
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		return config.Config{}, err
+	}
+
+	return cfg, nil
 }
